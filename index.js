b1931cb76f29e7e21567f4dfa250760539b150aa
James Sinker <j@Jamess-MacBook-Pro.local>
2019-12-15 17:55:32 -0800

    adds CORS access for front-end

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ let app = express();
 //Middleware
 app.use(express.json());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //check basic get request
 app.get('/', (req, res)=> {
   res.send('use /api/beaches to access all beaches');
